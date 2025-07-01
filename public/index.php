8210919080daea3165fc4d0363a7efa8e0504e00
Havlíček <havlicekl@seznam.cz>
2025-07-01 20:46:33 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 require_once "../app/core/App.php";
 require_once "../app/core/Controller.php";
 require_once "../app/core/Database.php";
-require_once "../app/core/Url";
+require_once "../app/core/Url.php";
 
 session_start();
 
