f26b8fc2b52c2569786aae7e96a23fca0fa42610
Havlíček <havlicekl@seznam.cz>
2025-07-04 19:05:43 +0200

    Stylování přihlášení
    Stylování stránky s přihlášením.

--- app/views/auth/signin.php
+++ app/views/auth/signin.php
@@ -15,32 +15,34 @@
     <link rel="stylesheet" href="<?= ASSETS ?>query/public/public-header-query.css" type="text/css"/>
     <link rel="stylesheet" href="<?= ASSETS ?>css/public/footer.css" type="text/css"/>
 
+    <link rel="stylesheet" href="<?= ASSETS ?>css/public/signin.css" type="text/css"/>
+
     <title><?= $data['title'] ?></title>
 </head>
 
 <body>
-<?php $this->view("header") ?>
+    <?php $this->view("header") ?>
 
-<main class="min-vh-100 d-flex flex-row justify-content-center align-items-center">
-    <section class="form">
-        <form action="" method="POST">
-            <h1>Přihlášení</h1>
-            <input type="email" name="log-email" class="email" placeholder="Email" value="<?= htmlspecialchars(isset($_POST['log-email']) ? $_POST['log-email']:  '') ?>" required><br />
-            <input type="password" name="log-password" class="email" placeholder="Heslo" required><br />
-            <?php if (!empty($data['errors'])): ?>
-                <ul>
-                    <?php foreach ($data['errors'] as $error): ?>
-                        <li><?= htmlspecialchars($error) ?></li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
-            <input type="submit" class="btn" value="Přihlásit">
-        </form>
-    </section>
-</main>
+    <main class="min-vh-100 d-flex flex-row justify-content-center align-items-center">
+        <section class="form">
+            <form action="" method="POST">
+                <h1>Přihlášení</h1>
+                <input type="email" name="log-email" class="email" placeholder="Email" value="<?= htmlspecialchars(isset($_POST['log-email']) ? $_POST['log-email']:  '') ?>" required><br />
+                <input type="password" name="log-password" class="password" placeholder="Heslo" required><br />
+                <?php if (!empty($data['errors'])): ?>
+                    <ul>
+                        <?php foreach ($data['errors'] as $error): ?>
+                            <li><?= htmlspecialchars($error) ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+                <input type="submit" class="btn" value="Přihlásit">
+            </form>
+        </section>
+    </main>
 
-<?php $this->view("footer") ?>
+    <?php $this->view("footer") ?>
 
-<script src="<?= ASSETS ?>js/bootstrap.js"></script>
+    <script src="<?= ASSETS ?>js/bootstrap.js"></script>
 </body>
 </html>
