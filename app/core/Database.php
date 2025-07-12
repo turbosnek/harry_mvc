d4ee4a1aefbc051fa0560d6104206c1c34628522
Havlíček <havlicekl@seznam.cz>
2025-07-12 19:10:35 +0200

    Update Database.php
    Logika pro připojení k databázi

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,1 +1,24 @@
 <?php
+
+
+class Database {
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pass = '';
+    private $dbname = 'harry_mvc';
+    protected $dbh;
+
+    public function __construct() {
+        try {
+            $this->dbh = new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf8", $this->user, $this->pass);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function query($sql, $params = []) {
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->execute($params);
+        return $stmt;
+    }
+}
