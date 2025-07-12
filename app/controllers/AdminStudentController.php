5e9b59ee53f0938cc8e9fc9a311b1a25a65f0979
Havlíček <havlicekl@seznam.cz>
2025-07-12 15:57:39 +0200

    Update AdminStudentController.php
    Vytvoření kontroly zpracování formuláře na přidání nového žáka do systému

--- app/controllers/AdminStudentController.php
+++ app/controllers/AdminStudentController.php
@@ -1,1 +1,33 @@
 <?php
+
+Class AdminStudentController extends Controller {
+    private Student $studentModel;
+
+    public function __construct() {
+        $this->studentModel = new Student();
+    }
+
+    public function createStudent(): void
+    {
+        $errors = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            $first_name = $_POST['first_name'];
+            $second_name = $_POST['second_name'];
+            $age = $_POST['age'];
+            $life = $_POST['life'];
+            $college = $_POST['college'];
+
+            $student = $this->studentModel->createStudent($first_name, $second_name, $age, $life, $college);
+
+            if ($student) {
+                Url::redirectUrl("/admin/students");
+            } else {
+                $errors[] = "Žáka se nepodařilo přidat do systému";
+            }
+        }
+
+        $this->view("auth/create-student", ['title' => "Administrace - Nový žák školy",
+            'errors' => $errors]);
+    }
+}
