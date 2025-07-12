dc84e749e131f40ea59970003af0b246a7c48e82
Havlíček <havlicekl@seznam.cz>
2025-07-12 19:32:09 +0200

    Update index.php
    Úprava pro správné cesty ke stylům

--- index.php
+++ index.php
@@ -6,4 +6,22 @@ require_once 'app/core/Database.php';
 
 session_start();
 
+spl_autoload_register(function($class) {
+    $paths = '../app/core' . $class . '.php';
+
+    foreach ($paths as $path) {
+        $file = $path . $class . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
+    }
+});
+
+$path = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'];
+$path = str_replace("index.php", "", $path);
+
+define('ROOT', $path);
+define('ASSETS', $path . "assets/");
+
 $app = new App();
