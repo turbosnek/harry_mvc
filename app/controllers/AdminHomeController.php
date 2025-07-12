69847401972ad9d11ee65388e3deb8985ae7ddd9
Havlíček <havlicekl@seznam.cz>
2025-07-12 18:21:56 +0200

    Smazání projektu
    Smazání celého projektu a začátek nového

--- app/controllers/AdminHomeController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class AdminHomeController extends Controller {
-    public function index() {
-        $this->view("admin/home/index", ["title" => "Administrace"]);
-    }
-}
--- app/controllers/LoginController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Class LoginController extends Controller {
-    public function index() {
-        $this->view("auth/signin", ["title" => "Přihlášení"]);
-    }
-}
