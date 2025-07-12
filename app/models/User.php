335383e3ff9f8156e26768ca16199dc726e0945c
Havlíček <havlicekl@seznam.cz>
2025-07-12 21:08:08 +0200

    Update User.php
    Metoda na vložení uživatele do databáze

--- app/models/User.php
+++ app/models/User.php
@@ -1,1 +1,35 @@
 <?php
+
+Class User extends Database {
+    public function __construct() {
+        $this->conn = $this->connect();
+    }
+
+    /**
+     * Registruje uživatele do systému
+     *
+     * @param $first_name - Křestní jméno uživatele
+     * @param $second_name - Příjmení uživatele
+     * @param $email - Email uživatele
+     * @param $password - Heslo uživatele
+     * @param $role - Role uživatele. Default ROLE_USER
+     *
+     * @return bool
+     */
+    public function register($first_name, $second_name, $email, $password, $role): bool {
+        $sql = "INSERT INTO user (first_name, second_name, email, password, role)
+                VALUES (:first_name, :second_name, :email, :password, :role)";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bindValue(":first_name", $first_name, PDO::PARAM_STR);
+        $stmt->bindValue(":second_name", $second_name, PDO::PARAM_STR);
+        $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+        $stmt->bindValue(":password", $password, PDO::PARAM_STR);
+        $stmt->bindValue(":role", $role, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        return true;
+    }
+}
