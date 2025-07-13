fc95be955795c01b5b90fc020f42c6b9c3d3d244
Havlíček <havlicekl@seznam.cz>
2025-07-13 21:51:15 +0200

    Create delete.php
    Vytvoření stránky a obsahu na smazání žáka

---
+++ app/views/admin/students/delete.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="cs-cz">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=""/> <!-- Popis stránky -->
+    <meta name="keywords" content=""/> <!-- Klíčová slova -->
+    <meta name="author" content=""/> <!-- Autor webu -->
+    <link rel="shortcut icon" href="images/ikona.ico"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/bootstrap.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/font-awesome.min.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/general.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/public/public-header.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>query/public/public-header-query.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/public/footer.css" type="text/css"/>
+
+    <link rel="stylesheet" href="<?= ASSETS ?>css/admin/admin-delete-student.css" type="text/css"/>
+
+    <title><?= $data['title'] ?></title>
+</head>
+
+<body>
+<?php $this->view("admin-header") ?>
+
+<main class="min-vh-100">
+    <?php if (!isset($_SESSION['role']) or !in_array($_SESSION['role'], ["ROLE_SUPER_ADMIN", "ROLE_ADMIN"])): ?>
+        <section class="security-error">
+            <h1>Nemáte dostatečná oprávnění k&nbsp;přístupu na tuto&nbsp;stránku.</h1>
+        </section>
+    <?php else: ?>
+        <section class="delete-form">
+            <?php if (!empty($data['errors'])): ?>
+                <?php foreach ($data['errors'] as $error): ?>
+                    <h1><?= htmlspecialchars($error) ?></h1>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <form action="" method="POST">
+                    <p>Jste si jistí, že chcete smazat žáka <?= $data['student']['first_name'] . " " . $data['student']['second_name'] ?>?</p>
+                    <div class="btns">
+                        <button>Smazat</button>
+                        <a href="/student/student/<?= $data['student']['id'] ?>"></a>
+                    </div>
+                </form>
+            <?php endif; ?>
+        </section>
+    <?php endif; ?>
+</main>
+
+<?php $this->view("footer") ?>
+
+<script src="<?= ASSETS ?>js/bootstrap.js"></script>
+</body>
+</html>
