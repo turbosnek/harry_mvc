37627324035b0937e22295693420ee691d2e8409
Havlíček <havlicekl@seznam.cz>
2025-07-13 17:50:26 +0200

    Úprava menu
    Úprava menu. Rozdělení do sekcí

--- app/views/admin-header.php
+++ app/views/admin-header.php
@@ -27,11 +27,14 @@
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <?php if(isset($_SESSION['id'])): ?>
                             <?php if ($_SESSION['role'] === "ROLE_SUPER_ADMIN" or $_SESSION['role'] === "ROLE_ADMIN"): ?>
-                                <li class="nav-item mx-2">
-                                    <a class="nav-link" href="/student/students">Seznam Žáků</a>
-                                </li>
-                                <li class="nav-item mx-2">
-                                    <a class="nav-link" href="/student/createStudent">Přidat Žáka</a>
+                                <li class="nav-item dropdown mx-2">
+                                    <a class="nav-link dropdown-toggle" href="#" id="studentDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Žáci
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-submenu" aria-labelledby="studentDropdown">
+                                        <li><a class="dropdown-item" href="/student/students">Seznam žáků</a></li>
+                                        <li><a class="dropdown-item" href="/student/createStudent">Přidat žáka</a></li>
+                                    </ul>
                                 </li>
                             <?php endif; ?>
                             <li class="nav-item mx-2">
