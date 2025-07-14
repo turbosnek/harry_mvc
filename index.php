2ebc0eac6f16f8a8a299ae690d080af7646ae125
Havlíček <havlicekl@seznam.cz>
2025-07-14 12:40:39 +0200

    Základní struktura
    Vytvoření základní struktury projektu

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'app/core/App.php';
+require_once 'app/core/Controller.php';
+require_once 'app/core/Database.php';
+session_start();
+
+$path = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'];
+$path = str_replace("index.php", "", $path);
+
+define('ROOT', $path);
+define('ASSETS', $path . "public/assets/");
+
+$app = new App();
