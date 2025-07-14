59af56300dfb1ac6162ff25bd63face3f84e3df0
Havlíček <havlicekl@seznam.cz>
2025-07-14 12:43:44 +0200

    Vytvoření logiky
    Vytvoření logiky na zobrazování stránek a přesměrování

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once 'app/core/App.php';
 require_once 'app/core/Controller.php';
 require_once 'app/core/Database.php';
+require_once 'app/core/Url.php';
 session_start();
 
 $path = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'];
