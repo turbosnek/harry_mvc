36143d8e3086a46d230997d82f0d9552447c586b
Havlíček <havlicekl@seznam.cz>
2025-07-18 23:41:04 +0200

    Oprava
    Oprava, abych mohl mít rozdělené controllery na Public a Admin

--- app/core/App.php
+++ app/core/App.php
@@ -4,16 +4,31 @@ class App {
     protected $controller = 'HomeController';
     protected $method = 'index';
     protected $params = [];
+    protected $controllerFolder = 'Public';
 
     public function __construct() {
         $url = $this->parseUrl();
 
-        if (file_exists('app/controllers/' . ucfirst($url[0]) . 'Controller.php')) {
-            $this->controller = ucfirst($url[0]) . 'Controller';
-            unset($url[0]);
+        if (isset($url[0])) {
+            $potentialController = ucfirst($url[0]) . 'Controller';
+
+            // Zkus Public
+            if (file_exists('app/controllers/Public/' . $potentialController . '.php')) {
+                $this->controller = $potentialController;
+                $this->controllerFolder = 'Public';
+                unset($url[0]);
+            }
+            // Zkus Admin
+            elseif (file_exists('app/controllers/Admin/' . $potentialController . '.php')) {
+                $this->controller = $potentialController;
+                $this->controllerFolder = 'Admin';
+                unset($url[0]);
+            }
         }
 
-        require_once 'app/controllers/' . $this->controller . '.php';
+        // Načtení správného controlleru podle složky
+        require_once 'app/controllers/' . $this->controllerFolder . '/' . $this->controller . '.php';
+
         $this->controller = new $this->controller;
 
         if (isset($url[1]) && method_exists($this->controller, $url[1])) {
