8bcaa1cd0d3898f0f1804e4d70d608c86c4f039f
Havlíček <havlicekl@seznam.cz>
2025-07-18 22:28:18 +0200

    Smazání
    Smazání projektu

--- app/views/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="custom-footer">
-    <div class="footer-container">
-        <p>&copy; 2025 - <?= date('Y') ?> Škola čar a kouzel v&nbsp;Bradavicích Lukáš&nbsp;Havlíček</p>
-    </div>
-</footer>
