c3c9a297cfa48b3417f319708f13a762d43f813a
Havlíček <havlicekl@seznam.cz>
2025-07-19 23:41:31 +0200

    Create delete.php
    Vytvoření stránky na mazání studenta

---
+++ app/views/admin/students/delete.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="cs-cz">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=""/> <!-- Popis stránky -->
+    <meta name="keywords" content=""/> <!-- Klíčová slova -->
+    <meta name="author" content=""/> <!-- Autor webu -->
+    <link rel="shortcut icon" href="images/ikona.ico"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/general.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>query/admin/general.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/admin/admin-header.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>query/admin/admin-header.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/footer.css" type="text/css"/>
+
+    <link rel="stylesheet" href="<?= ASSETS ?>css/admin/students/delete.css" type="text/css"/>
+
+    <title><?= $data['title'] ?></title>
+</head>
+
+<body>
+    <?php $this->view("admin/admin-header") ?>
+
+    <main>
+        <?php if (!isset($_SESSION['role']) or !in_array($_SESSION['role'], ["ROLE_SUPER_ADMIN", "ROLE_ADMIN"])): ?>
+            <section class="security-error">
+                <h1>Nemáte dostatečná oprávnění k&nbsp;přístupu na tuto&nbsp;stránku.</h1>
+            </section>
+        <?php else: ?>
+            <section class="delete-form">
+                <?php if (!empty($data['errors'])): ?>
+                    <?php foreach ($data['errors'] as $error): ?>
+                        <h1><?= htmlspecialchars($error) ?></h1>
+                    <?php endforeach; ?>
+                <?php else: ?>
+                    <form action="" method="POST">
+                        <input type="hidden" name="csrf_token" value="<?= $data['csrfToken'] ?? '' ?>">
+                        <p>Jste si jistí, že chcete smazat žáka <?= htmlspecialchars($data['student']['first_name']) . " " . htmlspecialchars($data['student']['second_name']) ?>?</p>
+                        <div class="btns">
+                            <button>Smazat</button>
+                            <a href="<?= ROOT ?>/admin/students/student/<?= $data['student']['id'] ?>">Zrušit</a>
+                        </div>
+                    </form>
+                <?php endif; ?>
+            </section>
+        <?php endif; ?>
+    </main>
+
+    <?php $this->view("footer") ?>
+
+    <script src="<?= ASSETS ?>js/header.js"></script>
+</body>
+</html>
