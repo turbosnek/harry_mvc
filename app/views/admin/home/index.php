e574f27a94b7afc66cd7374ee78656ad8171aae0
Havlíček <havlicekl@seznam.cz>
2025-07-19 20:05:09 +0200

    Create index.php
    Hlavní stránka administrace

---
+++ app/views/admin/home/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="cs-cz">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=""/> <!-- Popis stránky -->
+    <meta name="keywords" content=""/> <!-- Klíčová slova -->
+    <meta name="author" content=""/> <!-- Autor webu -->
+    <link rel="shortcut icon" href="images/ikona.ico"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/general.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>query/general.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/admin/admin-header.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>query/admin/admin-header.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>css/footer.css" type="text/css"/>
+
+    <link rel="stylesheet" href="<?= ASSETS ?>css/admin/home/index.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= ASSETS ?>query/admin/home/index.css" type="text/css"/>
+
+    <title><?= $data['title'] ?></title>
+</head>
+
+<body>
+    <?php $this->view("admin/admin-header") ?>
+
+    <main>
+        <?php if (!isset($_SESSION['role']) or !in_array($_SESSION['role'], ["ROLE_SUPER_ADMIN", "ROLE_ADMIN"])): ?>
+            <section class="security-error">
+                <h1>Nemáte dostatečná oprávnění k&nbsp;přístupu na tuto stránku.</h1>
+            </section>
+        <?php else: ?>
+            <section class="main-heading">
+                <h1>Vítejte v&nbsp;Administraci</h1>
+                <h2>Jsi přihlášen jako: <?= $_SESSION['first_name'] . "&nbsp;" . $_SESSION['second_name'] ?></h2>
+                <h2>Tvoje uživatelská role je: <?= str_replace(["ROLE_", "_"], "&nbsp;", $_SESSION['role']) ?></h2>
+            </section>
+        <?php endif; ?>
+    </main>
+
+    <?php $this->view("footer") ?>
+
+    <script src="<?= ASSETS ?>js/header.js"></script>
+</body>
+</html>
